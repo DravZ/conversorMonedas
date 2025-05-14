ffcb8e46b450e3a52b41c077b044d62b061b6d10
DravZ <cdgo345@gmail.com>
2025-05-14 00:08:37 -0600

    agregaron excceptions\

--- src/com/dravz/conversormonedas/vista/Principal.java
+++ src/com/dravz/conversormonedas/vista/Principal.java
@@ -2,6 +2,7 @@ package com.dravz.conversormonedas.vista;
 
 import com.dravz.conversormonedas.controlador.Conversor;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Principal {
@@ -15,67 +16,75 @@ public class Principal {
 
         System.out.println(" BIENVENIDO AL CONVERSOR DE MONEDAS ");
 
-        while (option != 7){
-            System.out.print("""
-                    ***************************************************************
-                    ------------------------------Menu-----------------------------
-                    1) Peso argentino > Dolar estadounidense
-                    2) Dolar estadounidense > Peso argentino
-                    3) Peso chileno > Dolar estadounidense
-                    4) Dolar estadounidense > Peso chileno
-                    5) Peso colombiano > Dolar estadounidense
-                    6) Dolar estadounidense > Peso colombiano
-                    7) Salir
-                    ***************************************************************
-                    Seleccione una opcion: 
-                    """);
+        while (option != 7) {
+            try {
+                System.out.print("""
+                        ***************************************************************
+                        ------------------------------Menu-----------------------------
+                        1) Peso argentino > Dolar estadounidense
+                        2) Dolar estadounidense > Peso argentino
+                        3) Peso chileno > Dolar estadounidense
+                        4) Dolar estadounidense > Peso chileno
+                        5) Peso colombiano > Dolar estadounidense
+                        6) Dolar estadounidense > Peso colombiano
+                        7) Salir
+                        ***************************************************************
+                        Seleccione una opcion: 
+                        """);
 
-            option = sc.nextInt();
+                option = sc.nextInt();
 
-            switch (option){
-                case 1:
-                    System.out.print("Ingrese la cantidad en pesos argentinos: ");
-                    cantidadConvertir = sc.nextDouble();
-                    System.out.printf("%.2f ARS = %.2f USD%n",
-                            cantidadConvertir,conversor.convertir("ARS", "USD", cantidadConvertir));
-                    break;
-                case 2:
-                    System.out.print("Ingrese la cantidad en dolares estadounidense: ");
-                    cantidadConvertir = sc.nextDouble();
-                    System.out.printf("%.2f USD = %.2f ARS%n",
-                            cantidadConvertir,conversor.convertir("USD", "ARS", cantidadConvertir));
-                    break;
-                case 3:
-                    System.out.print("Ingrese la cantidad en pesos chilenos: ");
-                    cantidadConvertir = sc.nextDouble();
-                    System.out.printf("%.2f CLP = %.2f USD%n",
-                            cantidadConvertir,conversor.convertir("CLP", "USD", cantidadConvertir));
-                    break;
-                case 4:
-                    System.out.print("Ingrese la cantidad en dolares estadounidenses: ");
-                    cantidadConvertir = sc.nextDouble();
-                    System.out.printf("%.2f USD = %.2f CLP%n",
-                            cantidadConvertir,conversor.convertir("USD", "CLP", cantidadConvertir));
-                    break;
-                case 5:
-                    System.out.print("Ingrese la cantidad en pesos colombianos: ");
-                    cantidadConvertir = sc.nextDouble();
-                    System.out.printf("%.2f COP = %.2f USD%n",
-                            cantidadConvertir,conversor.convertir("COP", "USD", cantidadConvertir));
-                    break;
-                case 6:
-                    System.out.print("Ingrese la cantidad en dolares estadounidense: ");
-                    cantidadConvertir = sc.nextDouble();
-                    System.out.printf("%.2f USD = %.2f COP%n",
-                            cantidadConvertir,conversor.convertir("USD", "COP", cantidadConvertir));
-                    break;
-                case 7:
-                    System.out.println("Gracias por usar nuestro sistema, hasta la proxima!");
-                    break;
-                default:
-                    System.out.println("ERROR: Opcion invalida.");
-                    break;
+                switch (option) {
+                    case 1:
+                        System.out.print("Ingrese la cantidad en pesos argentinos: ");
+                        cantidadConvertir = sc.nextDouble();
+                        System.out.printf("%.2f ARS = %.2f USD%n",
+                                cantidadConvertir, conversor.convertir("ARS", "USD", cantidadConvertir));
+                        break;
+                    case 2:
+                        System.out.print("Ingrese la cantidad en dolares estadounidense: ");
+                        cantidadConvertir = sc.nextDouble();
+                        System.out.printf("%.2f USD = %.2f ARS%n",
+                                cantidadConvertir, conversor.convertir("USD", "ARS", cantidadConvertir));
+                        break;
+                    case 3:
+                        System.out.print("Ingrese la cantidad en pesos chilenos: ");
+                        cantidadConvertir = sc.nextDouble();
+                        System.out.printf("%.2f CLP = %.2f USD%n",
+                                cantidadConvertir, conversor.convertir("CLP", "USD", cantidadConvertir));
+                        break;
+                    case 4:
+                        System.out.print("Ingrese la cantidad en dolares estadounidenses: ");
+                        cantidadConvertir = sc.nextDouble();
+                        System.out.printf("%.2f USD = %.2f CLP%n",
+                                cantidadConvertir, conversor.convertir("USD", "CLP", cantidadConvertir));
+                        break;
+                    case 5:
+                        System.out.print("Ingrese la cantidad en pesos colombianos: ");
+                        cantidadConvertir = sc.nextDouble();
+                        System.out.printf("%.2f COP = %.2f USD%n",
+                                cantidadConvertir, conversor.convertir("COP", "USD", cantidadConvertir));
+                        break;
+                    case 6:
+                        System.out.print("Ingrese la cantidad en dolares estadounidense: ");
+                        cantidadConvertir = sc.nextDouble();
+                        System.out.printf("%.2f USD = %.2f COP%n",
+                                cantidadConvertir, conversor.convertir("USD", "COP", cantidadConvertir));
+                        break;
+                    case 7:
+                        System.out.println("Gracias por usar nuestro sistema, HASTA LA PROXIMA!");
+                        break;
+                    default:
+                        System.out.println("ERROR: Opcion invalida.");
+                        break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Error al ingresar datos: " + e);
+                sc.nextLine();
+                option = 0;
             }
+
         }
+
     }
 }
